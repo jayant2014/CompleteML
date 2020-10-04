ed0dd4be38f249507b1e2eeb12d66cffac48561c
Test User <testuser@gmail.com>
2020-10-04 18:53:53 +0530

    Accuracy calculations

--- ml_utils/model_evaluation.py
+++ ml_utils/model_evaluation.py
@@ -2,9 +2,10 @@ def accuracy(y_true, y_pred):
     '''
     Function to calculate accuracy
     :param y_true : List of true values
-    :param y_pred : list of predicted values
-    :return : accuracy score
+    :param y_pred : List of predicted values
+    :return : Accuracy score
     '''
+
     ctr = 0
     for yt, yp in zip(y_true, y_pred):
         if yt == yp:
@@ -12,3 +13,81 @@ def accuracy(y_true, y_pred):
 
     # Return accuracy, correct predictions over number of samples
     return ctr / len(y_true)
+
+def true_positive(y_true, y_pred):
+    '''
+    Function to calculate true positive
+    :param y_true : List of true values
+    :param y_pred : List of predicted values
+    :return : Number of true positives
+    '''
+
+    tp = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 1 and yp == 1:
+            tp += 1
+
+    return tp
+
+def true_negative(y_true, y_pred):
+    '''
+    Function to calculate true negative
+    :param y_true : List of true values
+    :param y_pred : List of predicted values
+    :return : Number of true negatives
+    '''
+
+    tn = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 0 and yp == 0:
+            tn += 1
+
+    return tn
+
+def false_positive(y_true, y_pred):
+    '''
+    Function to calculate False positive
+    :param y_true : List of true values
+    :param y_pred : List of predicted values
+    :return : Number of false positives
+    '''
+
+    fp = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 0 and yp == 1:
+            fp += 1
+
+    return fp
+
+def false_negative(y_true, y_pred):
+    '''
+    Function to calculate false negative
+    :param y_true : List of true values
+    :param y_pred : List of predicted values
+    :return : Number of false negatives
+    '''
+
+    fn = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 1 and yp == 0:
+            fn += 1
+
+    return fn
+
+def accuracy_new(y_true, y_pred):
+    '''
+    Function to calculate accurayc using tp, tn, fp, fn
+    :param y_true : List of true values
+    :param y_pred : List of predicted values
+    :return : Accuracy score
+    '''
+
+    tp = true_positive(y_true, y_pred)
+    fp = false_positive(y_true, y_pred)
+    tn = true_negative(y_true, y_pred)
+    fn = false_negative(y_true, y_pred)
+
+    total_samples = tp + tn + fp + fn
+    accuracy = (tp + tn) / total_samples
+
+    return accuracy
---
+++ ml_utils/using_evaluation.py
@@ -0,0 +1,28 @@
+from sklearn import metrics
+import model_evaluation as mlu
+
+l1 = [0, 1, 1, 1, 0, 0, 0, 1]
+l2 = [0, 1, 0, 1, 0, 1, 0, 0]
+
+accuracy = mlu.accuracy(l1, l2)
+print("Accuracy")
+print("=================")
+print(accuracy)
+print("\n=================")
+print("Accuracy Using sklearn")
+print(metrics.accuracy_score(l1, l2))
+
+print("\n=================")
+print("Print TP, FP, TN, FN")
+tp = mlu.true_positive(l1, l2)
+print("True Positive : ", tp)
+fp = mlu.false_positive(l1, l2)
+print("False Positive : ", fp)
+tn = mlu.true_negative(l1, l2)
+print("True Negative : ", tn)
+fn = mlu.false_negative(l1, l2)
+print("False Negative : ", fn)
+print("\n=================")
+print("Accuracy using TP, FP, TN, FN")
+accuracy = mlu.accuracy_new(l1, l2)
+print("Accuracy : ", accuracy)
